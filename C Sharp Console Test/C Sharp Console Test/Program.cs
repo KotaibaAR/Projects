0a4cf467f8b5bf6451139e805b1b05e301752433
KotaibaAR <Kotaiba.AR@gmail.com>
2016-12-08 16:19:24 +0200

    my first program

---
+++ C Sharp Console Test/C Sharp Console Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Console_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //My First Program
+            Console.WriteLine("Hello Word!");
+            Console.ReadKey();
+        }
+    }
+}
